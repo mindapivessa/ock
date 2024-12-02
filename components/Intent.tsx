5d5086facda9c2357b861815475e6a7c18392f12
Mind Apivessa <apivessa.mind@gmail.com>
2024-12-02 22:53:03 +0700

    update font + fix dark mode for intent

--- components/Intent.tsx
+++ components/Intent.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { ArrowUp } from 'lucide-react'
 import { cn } from "@/lib/utils"
 import { Spinner } from '@/components/spinner'
+import { motion } from 'framer-motion'
 
 const promptTemplates = [
   "Bridge a token",
@@ -41,7 +42,7 @@ export function Intent() {
   const handleTemplateClick = (template: string) => {
     setIsPrefilling(true)
     setInput(template)
-    setTimeout(() => setIsPrefilling(false), 300) // Match this with the transition duration
+    setTimeout(() => setIsPrefilling(false), 500)
   }
 
   return (
@@ -52,21 +53,32 @@ export function Intent() {
           value={input}
           onChange={handleInputChange}
           className={cn(
-            "w-full px-4 py-2 pb-10 border border-zinc-300 dark:border-zinc-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:focus:ring-zinc-700 pr-10 h-24 resize-none transition-all duration-300 ease-in-out dark:bg-zinc-950 dark:text-white",
-            isPrefilling && "bg-zinc-100 dark:bg-zinc-800"
+            "w-full px-4 py-2 pb-10 border border-zinc-300 dark:border-zinc-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:focus:ring-zinc-700 pr-10 h-24 resize-none transition-all ease-in duration-300 dark:bg-zinc-950 dark:text-white placeholder:text-zinc-500 dark:placeholder:text-zinc-400",
+            isPrefilling && "text-opacity-50"
           )}
           disabled={isProcessing}
+          style={{ transition: 'color 0.3s ease-in' }}
         />
         {(input.length > 0 || isProcessing) && (
           <button
             type="submit"
             className={cn(
-              "absolute right-1.5 bottom-3 flex items-center justify-center bg-zinc-950 text-white rounded-lg w-[32px] h-[32px] transition-all duration-300 ease-in-out",
+              "absolute right-1.5 bottom-3 flex items-center justify-center dark:bg-white bg-black dark:text-black text-white rounded-lg w-[32px] h-[32px] transition-all duration-300 ease-in-out",
               isProcessing && "opacity-80"
             )}
           >
             {isProcessing ? (
-              <Spinner className="h-4 w-4" />
+              <motion.div
+                animate={{ rotate: 360 }}
+                transition={{
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  duration: 1.5,
+                  ease: "linear"
+                }}
+              >
+                <Spinner className="h-4 w-4 [&>circle]:opacity-25 [&>circle]:stroke-white/20 dark:[&>circle]:stroke-black/20 [&>circle]:stroke-[0.5]" />
+              </motion.div>
             ) : (
               <ArrowUp className="h-4 w-4" />
             )}
